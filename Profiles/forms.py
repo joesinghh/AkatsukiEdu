1edf483a4d03789a5a67b94ed051b1fdd23781e1
joesinghh <josingh481@gmail.com>
2022-01-22 17:50:19 +0530

    Fixed redirect issue

--- Profiles/forms.py
+++ Profiles/forms.py
@@ -1,13 +1,9 @@
-# from django.contrib.auth.models import User
 from .models import User
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django import forms
 from validate_email import validate_email
 from django.forms.widgets import PasswordInput,TextInput
 import requests
-
-
-
 
 class CreateUserForm(UserCreationForm):
     email=forms.EmailField(widget=TextInput(attrs={'placeholder':'Email'}),required=True)
@@ -24,13 +20,13 @@ class CreateUserForm(UserCreationForm):
             'is_teacher'
             
         ]
-    # def clean_email(self,*args,**kwargs):
-    #     cleaned_data = super(CreateUserForm, self).clean()
-    #     email = cleaned_data.get("email")
+    def clean_email(self,*args,**kwargs):
+        cleaned_data = super(CreateUserForm, self).clean()
+        email = cleaned_data.get("email")
         
-    #     response = requests.get("https://apilayer.net/api/check?access_key=a448482d0743db46d06084a7c97f32c9&email={}".format(email))
-    #     dic = response.json()
-    #     if dic['smtp_check']==True and dic['format_valid']==True and dic['mx_found']==True:
-    #         return email
-        # else:
-            # raise forms.ValidationError("Invalid Email")
+        response = requests.get("https://apilayer.net/api/check?access_key=a448482d0743db46d06084a7c97f32c9&email={}".format(email))
+        dic = response.json()
+        if dic['smtp_check']==True and dic['format_valid']==True and dic['mx_found']==True:
+            return email
+        else:
+            raise forms.ValidationError("Invalid Email")
